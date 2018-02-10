a5ad5223c2859d99eb0a32815bf3b4d0e0c1d831
Shterion <shterion.yanev@yahoo.com>
2018-02-10 19:51:25 +0100

    Create users routes

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const {User} = require('./../models/User');
 
 // Register
-router.get('/register', (req, res) => {
-  res.send('Register ...')
+// router.post('/register', (req, res, next) => {
+//   let newUser = new User {
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password
+//   };
+//
+//   newUser.addUser(newUser).then.((user) => {
+//     res.json({success: true, msg: 'User registered'})
+//     console.log(user);
+//   }).catch((e) => {
+//     res.json({success: false, msg: 'Failed to register user'});
+//     console.log(e);
+//   });
+//
+// });
+
+
+router.post('/register', async (req, res) => {
+  let newUser = new User ({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+  let user = await User.addUser(newUser);
+  res.redirect('/users/profile');
 });
 
+
+
 // Authenticate
-router.get('/authenticate', (req, res) => {
+router.post('/authenticate', (req, res, next) => {
   res.send('Authenticate ...')
 });
 
 // Profile
-router.get('/profile', (req, res) => {
+router.get('/profile', (req, res, next) => {
   res.send('Profile ...')
-});
-
-// Validate
-router.get('/validate', (req, res) => {
-  res.send('Validate ...')
 });
 
 module.exports = router;
