dc516161e00793e08f04c6edfbf03fb50a3e862d
Shterion <shterion.yanev@yahoo.com>
2018-02-13 14:01:45 +0100

    Create validate service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { ValidateService } from './services/validate.service';
+
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
@@ -36,9 +39,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service'
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private _validateService: ValidateService, private _flashMessagesService: FlashMessagesService) { }
 
   ngOnInit() {
   }
 
+  onRegisterSubmit() {
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+
+    // Required fields
+    if(!this._validateService.validateRegister(user)) {
+      this._flashMessagesService.show('Please fill in all fields!', { cssClass: 'alert-danger', timeout: 3000 });
+      return false;
+    }
+
+    // Validate email
+    if(!this._validateService.validateEmail(user.email)) {
+      this._flashMessagesService.show('Please ener a valid email!', { cssClass: 'alert-danger', timeout: 1000 });
+      return false;
+    }
+  }
 }
