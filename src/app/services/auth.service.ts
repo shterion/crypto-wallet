274c6c54dfdaa556a7addb9e0cfd64d8da1c723d
Shterion <shterion.yanev@yahoo.com>
2018-02-14 20:26:03 +0100

    Create an authentication service for registration

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private _http: Http) { }
+
+  registerUser(user) {
+    let headers = new Headers();
+    headers.append('ContentType', 'application/json');
+    return this._http.post('http://localhost:3000/users/register', user, {headers})
+      .map(res => res.json());
+  }
+}
