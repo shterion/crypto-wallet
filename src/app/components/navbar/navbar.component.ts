92897645d620b80f829b9e2eeb8b9344587320d4
Shterion <shterion.yanev@yahoo.com>
2018-02-15 11:17:01 +0100

    Create logout function and delete data from local storage

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _flashMessagesService: FlashMessagesService
+  ) { }
 
   ngOnInit() {
   }
 
+  onLogoutClick() {
+    this._authService.logout();
+    this._flashMessagesService.show('You are logged out!', { cssClass: 'alert-success', timeout: 3000});
+    this._router.navigate(['/login']);
+    return false;
+  }
 }
