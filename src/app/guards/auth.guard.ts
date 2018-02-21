c9a10ccd37a6ea4fb40d3bf09ff57c4071a04bf9
Shterion <shterion.yanev@yahoo.com>
2018-02-21 19:40:42 +0100

    Add flash messages for auth guard before redirect

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private _router: Router, private _authService: AuthService) {
-
-  }
+  constructor(
+    private _router: Router,
+    private _authService: AuthService,
+    private _flashMessagesService: FlashMessagesService
+  ) {}
 
   canActivate() {
     if (this._authService.loggedIn()) {
       return true;
     } else {
+      this._flashMessagesService.show('You have to be logged in to access this page!', {cssClass: 'alert-danger', timeout: 5000});
       this._router.navigate(['/login']);
       return false;
     }
