dd6fd203d2f4cb2cf371af9df296f00f28220bab
Shterion <shterion.yanev@yahoo.com>
2018-02-22 20:03:20 +0100

    Finishthe initial coin model

--- models/Coin.js
+++ models/Coin.js
@@ -3,7 +3,58 @@ const mongoose = require('mongoose');
 const Schema = mongoo.Schema;
 
 const CoinSchema = new Schema({
-
+  name: {
+    type: String,
+    required: true
+  },
+  symbol: {
+    type: String,
+    required: true
+  },
+  rank: {
+    type: Number,
+    required: true
+  },
+  priceUsd: {
+    type: Number,
+    required: true
+  },
+  priceBtc: {
+    type: Number,
+    required: true
+  },
+  twentyfourHourVolumeUsd: {
+    type: Number,
+    required: true
+  },
+  marketCapUsd: {
+    type: Number,
+    required: true
+  },
+  availableSupply: {
+    type: Number,
+    required: true
+  },
+  totalSuply: {
+    type: Number,
+    required: true
+  },
+  percentChangeOneHour: {
+    type: Number,
+    required: true
+  },
+  percentChangeTwentyFourHour: {
+    type: Number,
+    required: true
+  },
+  percentChangeOneWeek: {
+    type: Number,
+    required: true
+  },
+  lastUpdated: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 let Coin = mongoose.model('Coin', CoinSchema);
